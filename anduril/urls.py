8e630055c342699e805621eee74cf77ccd825832
Noah Kim <noahbkim@gmail.com>
2017-06-29 14:21:34 -0700

    Fix bug caused by trying to be fancy.

--- anduril/urls.py
+++ anduril/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.conf.urls import url, include
 from django.contrib import admin
-import home
+import home.site
 
 
 urlpatterns = [
--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from core import models
 
 
 def login(request):
     """Login page for the home interface."""
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        print(username, password)
 
     return render(request, "home/login.html")
