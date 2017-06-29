b393b2b46304b2b5729824cc0ad4a7f8859ad18c
Noah Kim <noahbkim@gmail.com>
2017-06-29 13:13:33 -0700

    Add several templates and begin porting styles.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    """Login page for the home interface."""
+
+    return render(request, "home/login.html")
---
+++ static/js/parameters.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Noah on 6/29/17.
+ */
