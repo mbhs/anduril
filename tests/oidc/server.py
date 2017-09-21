af970c6d17ee531976171aaebf76772655fb2295
Noah Kim <noahbkim@gmail.com>
2017-09-20 22:45:55 -0400

    Add OIDC tests

---
+++ tests/oidc/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_oidc import OpenIDConnect
+
+app = Flask(__name__)
+app.secret_key = "secret"
+app.config.update(OIDC_CLIENT_SECRETS="./secrets.json",
+                  OIDC_SCOPES=["openid", "user", "email"])
+
+oidc = OpenIDConnect(app)
+
+@app.route("/")
+def index():
+    if not oidc.user_loggedin:
+        return """<a href="/login">Log in</a>"""
+    else:
+        return f"""Hello, {oidc.user_getfield('username')}"""
+
+@app.route('/login')
+@oidc.require_login
+def login():
+    return "Login"
+
+if __name__ == "__main__":
+    app.run()
