d20ffab3c8d04993be18a5032ac677e1ff9ca193
Noah Kim <noahbkim@gmail.com>
2017-09-21 14:42:17 -0400

    Revert "OIDC example works, issuer must have double slash?"
    This reverts commit b7d919294123ec7a683ba3b6fbbc2b0e98467a34.

--- tests/oidc/server.py
+++ tests/oidc/server.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 from flask_oidc import OpenIDConnect
 
 app = Flask(__name__)
 app.secret_key = "secret"
 app.config.update(OIDC_CLIENT_SECRETS="./secrets.json",
-                  OIDC_SCOPES=["openid", "profile", "email"],
-                  OIDC_ID_TOKEN_COOKIE_SECURE=False)
+                  OIDC_SCOPES=["openid", "user", "email"])
 
 oidc = OpenIDConnect(app)
 
@@ -15,23 +14,13 @@ def index():
     if not oidc.user_loggedin:
         return """<a href="/login">Log in</a>"""
     else:
-        first_name = oidc.user_getfield('first_name')
-        last_name = oidc.user_getfield('last_name')
-        username = oidc.user_getfield('username')
-        return f"""Hello, {first_name} {last_name}, {username}<br><a href="/logout">Log out</a>"""
+        return f"""Hello, {oidc.user_getfield('username')}"""
 
 
 @app.route('/login')
 @oidc.require_login
 def login():
-    return redirect("/")
-
-
-@app.route('/logout')
-def logout():
-    oidc.logout()
-    return redirect("/")
-
+    return "Login"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
