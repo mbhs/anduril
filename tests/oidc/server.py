b7d919294123ec7a683ba3b6fbbc2b0e98467a34
Noah Kim <noahbkim@gmail.com>
2017-09-21 13:39:07 -0400

    OIDC example works, issuer must have double slash?

--- tests/oidc/server.py
+++ tests/oidc/server.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_oidc import OpenIDConnect
 
 app = Flask(__name__)
 app.secret_key = "secret"
 app.config.update(OIDC_CLIENT_SECRETS="./secrets.json",
-                  OIDC_SCOPES=["openid", "user", "email"])
+                  OIDC_SCOPES=["openid", "profile", "email"],
+                  OIDC_ID_TOKEN_COOKIE_SECURE=False)
 
 oidc = OpenIDConnect(app)
 
@@ -14,13 +15,23 @@ def index():
     if not oidc.user_loggedin:
         return """<a href="/login">Log in</a>"""
     else:
-        return f"""Hello, {oidc.user_getfield('username')}"""
+        first_name = oidc.user_getfield('first_name')
+        last_name = oidc.user_getfield('last_name')
+        username = oidc.user_getfield('username')
+        return f"""Hello, {first_name} {last_name}, {username}<br><a href="/logout">Log out</a>"""
 
 
 @app.route('/login')
 @oidc.require_login
 def login():
-    return "Login"
+    return redirect("/")
+
+
+@app.route('/logout')
+def logout():
+    oidc.logout()
+    return redirect("/")
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
